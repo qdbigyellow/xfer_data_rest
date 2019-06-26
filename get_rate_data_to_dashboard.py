f6e75d2d5ce2f0001fd8aaeccc8930f02c73521a
jim <bigyellow@gmail.com>
2019-06-26 19:54:08 +0200

    update the matrix name

--- get_rate_data_to_dashboard.py
+++ get_rate_data_to_dashboard.py
@@ -5,7 +5,7 @@ from lib.tsdb.prom import push_data_to_gateway
 
 def transfer_data_to_dashboard():
     for data in fetch_data():
-        if len(data) > 0 and "rate" in data:
+        if len(data) > 0 and "rate" in data[0]:
             process_loan_rate_data(data)        
 
 def fetch_data():
@@ -17,7 +17,7 @@ def process_loan_rate_data(data):
     for loan in data:
         rate = loan["rate"].replace("*&nbsp;", "").replace(",", ".")
         afdragsfri = loan["repaymentFreedomMax"]
-        name = loan["fundName"]
+        name = "loan" + loan["fundName"].replace(" ", "_").replace(",", "_").replace("%", "_").replace("__", "_").lower()
         period = loan["loanPeriodMax"]
 
         if float(rate) < 100 and afdragsfri.lower() == "nej":
