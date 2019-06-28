407db533b7608d883ad168ae40cba808befded26
Jinxu Huang <jinh@oticon.com>
2019-06-28 09:34:56 +0200

    add another way to replace all non-alphanumeric

--- get_rate_data_to_dashboard.py
+++ get_rate_data_to_dashboard.py
@@ -2,6 +2,7 @@ from config import Configurations
 import json
 import urllib.request as request
 from lib.tsdb.prom import push_data_to_gateway
+import re
 
 def transfer_data_to_dashboard():
     for data in fetch_data():
@@ -18,9 +19,14 @@ def process_loan_rate_data(data):
     for loan in data:
         rate = loan["rate"].replace("*&nbsp;", "").replace(",", ".")
         afdragsfri = loan["repaymentFreedomMax"]
-        name = "loan_" + loan["fundName"].replace(" ", "_").replace(",", "_").replace("%", "_").replace("__", "_").lower()
+        name = "loan_" + loan["fundName"].replace(" ", "_").replace(",", "_").replace("%", "_").replace("__", "_").lower()        
         period = loan["loanPeriodMax"]
         print(f"matrics {name}")
 
         if float(rate) < 100 and afdragsfri.lower() == "nej":
             push_data_to_gateway(job_name=name, gauge_name="nordea_loan_rate", gauge_detail=period, data=rate)
+
+def replace_non_alphanumeric(data):
+    pattern= re.compile(r'\W+')
+    pattern.sub('_', data)    
+    
