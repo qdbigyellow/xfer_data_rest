d20bf85d400cdee707094260ef9824ec19c5a432
Jim Wong <bigyellow@gmail.com>
2020-01-16 22:37:49 +0100

    reorganiza the code, and use pip3 in action

--- financial_modeling/analysing_toolkit.py
+++ financial_modeling/analysing_toolkit.py
@@ -8,6 +8,7 @@ import asyncio
 import time
 import aiohttp
 import psycopg2
+from lib.pg_connector import conn, insert_query
 
 PROFIT_LIST = [
     'Gross Profit Growth',
@@ -34,7 +35,7 @@ CASHFLOW_LIST = [
 
 
 def plot_data(symbol, data: pd.DataFrame, matrix):
-    plt.plot(data.index.values, data[matrix], label=matrix)
+    plt.plot(data.index[::-1].values, data[matrix][::-1], label=matrix)
 
 
 def plot_datas(symbol, matrices):
@@ -79,35 +80,29 @@ def plot(symbols, matrices):
         plot_datas(symbols[0], list(matrices))
 
 
-available_matrix = [SupportKeys.pb_ratio, SupportKeys.pe_ratio, SupportKeys.ebit_growth, SupportKeys.eps_growth, SupportKeys.free_cash_flow_growth]
-
-@main.command(name='find')
-@click.option('-m', '--matrix', type=str, help="Matrix name", required=True)
-@click.option('-u', '--update-datasource', type=bool, help="Update the datasource file", default=False,  required=False)
-def find(matrix, update_datasource=False, number=20, sort='desc'):
-    #TODO: Get data from database, and sort in database instead here. 
-    print(data)
-
-
 @main.command('dbextract')
 def dbextract():
-    sites = get_remote_symbol_list()
+    """Read the give matrix from all available symbols and put the data into postgresql database.  So Grafana can query the database directly from there.
+    """
+    symbols = get_remote_symbol_list()
     start_time = time.time()
-    a = asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
+    # a = asyncio.get_event_loop().run_until_complete(download_all_symbols(symbols))
+    asyncio.run(download_all_symbols(symbols))
     duration = time.time() - start_time
-    print(f"Downloaded {len(sites)} sites in {duration} seconds")
+    print(f"Downloaded {len(symbols)} symbols in {duration} seconds")
 
 
-async def download_all_sites(sites):
+async def download_all_symbols(symbols):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for symbol in sites:
-            task = asyncio.ensure_future(download_site(session, symbol))
+        for symbol in symbols:
+            # task = asyncio.ensure_future(download_financial_data(session, symbol))
+            task = asyncio.create_task(download_financial_data(session, symbol))
             tasks.append(task)
-        return await asyncio.gather(*tasks, return_exceptions=True)
+        # Note use * before tasks to unpack.
+        await asyncio.gather(*tasks, return_exceptions=True)
 
-async def download_site(session, symbol, index=0):
-    print(symbol)
+async def download_financial_data(session, symbol, index=0):
     url = f'https://financialmodelingprep.com/api/v3/company-key-metrics/{symbol}?period=quarter'
     async with session.get(url) as response:
         key_metrics = await response.json()
@@ -116,26 +111,9 @@ async def download_site(session, symbol, index=0):
     async with session.get(url) as response:
         growth = await response.json()    
 
-    pb_ratio = 0.0
-    pe_ratio = 0.0
-    if len(key_metrics) > 0:
-        pb_ratio = float(key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value]) if key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value] is not "" else 0.0
-        pe_ratio = float(key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value]) if key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value] is not "" else 0.0
-    eps_growth = 0.0    
-    ebit_growth = 0.0    
-    fcf_growth = 0.0    
-    if len(growth) > 0:
-        ebit_growth = float(growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value]) if growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value] is not "" else 0.0
-        eps_growth = float(growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value]) if growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value] is not "" else 0.0
-        fcf_growth = float(growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value]) if growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value] is not "" else 0.0
+    pb_ratio, pe_ratio, eps_growth, ebit_growth, fcf_growth = read_data(key_metrics, growth)
 
-    connection = psycopg2.connect(user = "autouser",
-                            password = "autouser",
-                            host = "192.168.0.6",
-                            port = "5432",
-                            database = "homeauto")
-    cursor = connection.cursor()
-
+    connection = conn()
     pg_insert_query = """INSERT INTO public.financial_matrix ("symbol", "EPS_Growth", "FCF_Growth", "PB_Ratio", "PE_Ratio", "EBIT_Growth") VALUES (%s,%s,%s,%s,%s,%s)
        ON CONFLICT ("symbol")
        DO
@@ -147,10 +125,24 @@ async def download_site(session, symbol, index=0):
           "EBIT_Growth" = EXCLUDED."EBIT_Growth";
     """
     record_to_insert = (symbol, eps_growth, fcf_growth, pb_ratio, pe_ratio, ebit_growth)
-    cursor.execute(pg_insert_query, record_to_insert)
-    connection.commit()
-    cursor.close()
+    insert_query(connection=connection, query=pg_insert_query, data=record_to_insert)
     connection.close()
+
+
+def read_data(key_metrics, growth):
+    pb_ratio = 0.0
+    pe_ratio = 0.0
+    eps_growth = 0.0    
+    ebit_growth = 0.0    
+    fcf_growth = 0.0    
+    if len(key_metrics) > 0:
+        pb_ratio = float(key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value]) if key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value] is not "" else 0.0
+        pe_ratio = float(key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value]) if key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value] is not "" else 0.0
+    if len(growth) > 0:
+        ebit_growth = float(growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value]) if growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value] is not "" else 0.0
+        eps_growth = float(growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value]) if growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value] is not "" else 0.0
+        fcf_growth = float(growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value]) if growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value] is not "" else 0.0
+    return pb_ratio, pe_ratio, eps_growth, ebit_growth, fcf_growth
 
 #%%
 
--- lib/pg_connector.py
+++ lib/pg_connector.py
@@ -1,12 +1,11 @@
 import psycopg2
-import 
 
-def conn():
-    connection = psycopg2.connect(user = "autouser",
-                                password = "autouser",
-                                host = "192.168.0.6",
-                                port = "5432",
-                                database = "homeauto")
+def conn(user="autouser", password="autouser", host="192.168.0.6", port="5432", database="homeauto"):
+    connection = psycopg2.connect(user = user,
+                                password = password,
+                                host = host,
+                                port = port,
+                                database = database)
     return connection
 
 
@@ -18,5 +17,31 @@ def insert_query(connection, query, data):
 
     
 
+async def a_conn():
+    """
+    Not finish. a connection for asyncio
+    """
 
+    connection = psycopg2.connect(user = "autouser",
+                            password = "autouser",
+                            host = "192.168.0.6",
+                            port = "5432",
+                            database = "homeauto")
+    cursor = connection.cursor()
+
+    pg_insert_query = """INSERT INTO public.financial_matrix ("symbol", "EPS_Growth", "FCF_Growth", "PB_Ratio", "PE_Ratio", "EBIT_Growth") VALUES (%s,%s,%s,%s,%s,%s)
+       ON CONFLICT ("symbol")
+       DO
+          UPDATE 
+          SET "EPS_Growth" = EXCLUDED."EPS_Growth",
+          "FCF_Growth" = EXCLUDED."FCF_Growth",
+          "PB_Ratio" = EXCLUDED."PB_Ratio",
+          "PE_Ratio" = EXCLUDED."PE_Ratio",
+          "EBIT_Growth" = EXCLUDED."EBIT_Growth";
+    """
+    record_to_insert = (symbol, eps_growth, fcf_growth, pb_ratio, pe_ratio, ebit_growth)
+    cursor.execute(pg_insert_query, record_to_insert)
+    connection.commit()
+    cursor.close()
+    connection.close()
 
