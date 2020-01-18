4531cb0bb6d560c0734181c4ab0d7389e13fa478
Jim Wong <bigyellow@gmail.com>
2020-01-18 22:44:00 +0100

    fix error

--- financial_modeling/tests/company_data_to_dstb_test.py
+++ financial_modeling/tests/company_data_to_dstb_test.py
@@ -6,9 +6,9 @@ class TestCompanyDataToDSTB:
 
     def test_gauge_dict_return_defaultvalue(self):
         d = gauge_dict()
-        assert "see gauge name" == g_dict["anything"] 
-        assert "Gross Profit Groth" == g_dict["gross_profit_growth"]
-        assert "Share price to Book value. Less than one means share is under valued" == g_dict['pb_ratio']
+        assert "see gauge name" == d["anything"] 
+        assert "Gross Profit Groth" == d["gross_profit_growth"]
+        assert "Share price to Book value. Less than one means share is under valued" == d['pb_ratio']
 
 
 
