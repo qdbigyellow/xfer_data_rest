2f704ae49bdbb65422f1615d89f9e3f00c2a4d6b
Jim Wong <bigyellow@gmail.com>
2020-01-18 22:39:54 +0100

    add a unit test

--- financial_modeling/tests/company_data_to_dstb_test.py
+++ financial_modeling/tests/company_data_to_dstb_test.py
@@ -1,3 +1,15 @@
+from financial_modeling.company_data_to_tsdb import gauge_dict
+
 class TestCompanyDataToDSTB:
     def test_pass(self):
         assert True
+
+    def test_gauge_dict_return_defaultvalue(self):
+        d = gauge_dict()
+        assert "see gauge name" == g_dict["anything"] 
+        assert "Gross Profit Groth" == g_dict["gross_profit_growth"]
+        assert "Share price to Book value. Less than one means share is under valued" == g_dict['pb_ratio']
+
+
+
+
