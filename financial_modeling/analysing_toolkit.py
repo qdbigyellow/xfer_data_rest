22aec0377c27116ea0ca302fd6990b6e1653f47c
Jim Wong <bigyellow@gmail.com>
2020-02-24 21:55:59 +0100

    fix pip8 error,  and include .vscode folder

--- financial_modeling/analysing_toolkit.py
+++ financial_modeling/analysing_toolkit.py
@@ -2,7 +2,7 @@ from financial_modeling.company_data_to_tsdb import get_company_growth, get_remo
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-import click 
+import click
 from os import path
 import asyncio
 import time
@@ -45,6 +45,7 @@ def plot_datas(symbol, metrics):
     plt.legend(loc="upper left")
     plt.show()
 
+
 def get_data(symbol, matrix: Keys):
     matrix_data = []
     data = get_remote_data(symbol=symbol, report=matrix.report)
@@ -56,16 +57,16 @@ def get_data(symbol, matrix: Keys):
 def cross_companies_compare(symbols, matrix):
     for s in symbols:
         d = get_data(s, matrix)
-        plt.plot(d, label=s)        
+        plt.plot(d, label=s)
     plt.legend(loc="upper left")
     plt.title(f'Compare {matrix.value}')
     plt.show()
-    
-        
+
 @click.group(context_settings={'help_option_names': ['-h', '--help']},
              help='Analysis the company financial figures')
 def main():
     pass
+
 
 @main.command(name='plot', help="plot the matrix on screen")
 @click.option('-s', '--symbols', type=str, help="The symbol of compaines to anaylisis", multiple=True)
@@ -97,10 +98,12 @@ async def download_all_symbols(symbols):
         tasks = []
         for symbol in symbols:
             # task = asyncio.ensure_future(download_financial_data(session, symbol))
-            task = asyncio.create_task(download_financial_data(session, symbol))
+            task = asyncio.create_task(
+                download_financial_data(session, symbol))
             tasks.append(task)
         # Note use * before tasks to unpack.
         await asyncio.gather(*tasks, return_exceptions=True)
+
 
 async def download_financial_data(session, symbol, index=0):
     url = f'https://financialmodelingprep.com/api/v3/company-key-metrics/{symbol}?period=quarter'
@@ -109,9 +112,10 @@ async def download_financial_data(session, symbol, index=0):
 
     url = f'https://financialmodelingprep.com/api/v3/financial-statement-growth/{symbol}?period=quarter'
     async with session.get(url) as response:
-        growth = await response.json()    
+        growth = await response.json()
 
-    pb_ratio, pe_ratio, eps_growth, ebit_growth, fcf_growth = read_data(key_metrics, growth)
+    pb_ratio, pe_ratio, eps_growth, ebit_growth, fcf_growth = read_data(
+        key_metrics, growth)
 
     connection = conn()
     pg_insert_query = """INSERT INTO public.financial_matrix ("symbol", "EPS_Growth", "FCF_Growth", "PB_Ratio", "PE_Ratio", "EBIT_Growth") VALUES (%s,%s,%s,%s,%s,%s)
@@ -124,28 +128,34 @@ async def download_financial_data(session, symbol, index=0):
           "PE_Ratio" = EXCLUDED."PE_Ratio",
           "EBIT_Growth" = EXCLUDED."EBIT_Growth";
     """
-    record_to_insert = (symbol, eps_growth, fcf_growth, pb_ratio, pe_ratio, ebit_growth)
-    insert_query(connection=connection, query=pg_insert_query, data=record_to_insert)
+    record_to_insert = (symbol, eps_growth, fcf_growth,
+                        pb_ratio, pe_ratio, ebit_growth)
+    insert_query(connection=connection,
+                 query=pg_insert_query, data=record_to_insert)
     connection.close()
 
 
 def read_data(key_metrics, growth, index=0):
     pb_ratio = 0.0
     pe_ratio = 0.0
-    eps_growth = 0.0   
-    ebit_growth = 0.0    
-    fcf_growth = 0.0    
+    eps_growth = 0.0
+    ebit_growth = 0.0
+    fcf_growth = 0.0
     if len(key_metrics) > 0:
-        pb_ratio = float(key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value]) if key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value] != "" else 0.0
-        pe_ratio = float(key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value]) if key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value] != "" else 0.0
+        pb_ratio = float(key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value]
+                         ) if key_metrics[SupportKeys.pb_ratio.path][index][SupportKeys.pb_ratio.value] != "" else 0.0
+        pe_ratio = float(key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value]
+                         ) if key_metrics[SupportKeys.pe_ratio.path][index][SupportKeys.pe_ratio.value] != "" else 0.0
     if len(growth) > 0:
-        ebit_growth = float(growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value]) if growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value] != "" else 0.0
-        eps_growth = float(growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value]) if growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value] != "" else 0.0
-        fcf_growth = float(growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value]) if growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value] != "" else 0.0
+        ebit_growth = float(growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value]
+                            ) if growth[SupportKeys.ebit_growth.path][index][SupportKeys.ebit_growth.value] != "" else 0.0
+        eps_growth = float(growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value]
+                           ) if growth[SupportKeys.eps_growth.path][index][SupportKeys.eps_growth.value] != "" else 0.0
+        fcf_growth = float(growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value]
+                           ) if growth[SupportKeys.free_cash_flow_growth.path][index][SupportKeys.free_cash_flow_growth.value] != "" else 0.0
     return pb_ratio, pe_ratio, eps_growth, ebit_growth, fcf_growth
 
-#%%
 
-
+# %%
 if __name__ == "__main__":
     main()
